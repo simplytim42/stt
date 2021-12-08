0626568e03bbfe297f7d27b89b7cec0f16fd3147
Tim Mackay <simplytim42@gmail.com>
2021-12-08 09:59:13 +0000

    converted md to php page

---
+++ resources.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Resources</title>
+    <?php require './includes/headContent.html' ?>
+</head>
+<body>
+
+    <?php require './includes/nav.html' ?>
+
+    <main>
+        <section>
+            <h1>Resources</h1>
+            <h2>Research</h2>
+            <ul>
+                <li>google.com</li>
+                <li>php.net</li>
+                <li>stackoverflow.com</li>
+                <li>developer.mozilla.org</li>
+                <li>json.org</li>
+                <li>css-tricks.com</li>
+                <li>wsschools.com</li>
+            </ul>
+
+            <h2>Open Source Code</h2>
+            <ul>
+                <li>Google Charts API</li>
+                <li>Composer Package Manager (for Class Autoloading)</li>
+            </ul>
+        </section>
+    </main>
+
+</body>
+</html>
